93be3eef43613a67a669c1082e814417ee211a70
Grace Choi <oiograceoio@gmail.com>
2019-01-07 06:37:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
       :equilateral
     elsif side1 == side2 || side2 == side3 || side3 == side1
       :isosceles
-    elsif
+    elsif side1 != side2 && side2 != side3 && side3 != side1
       :scalene
     else
       raise TriangleError
