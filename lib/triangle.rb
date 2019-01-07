730eeb995807b1722d2cc2acccb70a7a9ef91709
Grace Choi <oiograceoio@gmail.com>
2019-01-07 06:44:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,5 @@
 class Triangle
-  attr_accessor :side1, :side2, :side3
+  attr_reader :side1, :side2, :side3
 
   def initialize(side1, side2, side3) # write code here
     @side1 = side1
@@ -8,16 +8,22 @@ class Triangle
   end
 
   def kind
+    real_triangle
     if side1 == side2 && side2 == side3
       :equilateral
     elsif side1 == side2 || side2 == side3 || side3 == side1
       :isosceles
-    elsif side1 != side2 && side2 != side3 && side3 != side1
-      :scalene
     else
-      raise TriangleError
+      :scalene
     end
   end
+
+  def real_triangle
+    real = [(side1 + side2 > side3), (side2 + side3 < side1), (side3 + side1 > side2)]
+    [side1, side2, side3].each {|side| real << false if side <= 0}
+    raise TriangleError if real.include?(false)
+  end
+    
 
   class TriangleError < StandardError
   end
